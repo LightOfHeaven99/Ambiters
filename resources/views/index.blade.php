e78fb5a52db802a0dcd3ab5a8fc8577534fd770d
F4dr0 <41900341+F4dr0@users.noreply.github.com>
2019-05-14 14:27:49 +0200

    Cały Laravel. Ominałem już ogarnianie folderu Node i po prostu mamy tyle zeby dzialac. Powinno na chillku starczyc i pewnie 70% contentu tutaj nie tkniemy. Pliki ktore beda wyswietlane znajduja sie w resources/views.

---
+++ resources/views/index.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pl">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Ambiters</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Carrois+Gothic+SC" rel="stylesheet">
+
+        <!-- Styles -->
+
+        <link rel="stylesheet" href="css/bootstrap.css" type="text/css">
+        <link rel="stylesheet" href="css/ambiters.css" type="text/css">
+
+
+        <!-- JS -->
+        <script src='js/jquery-3.1.1.min.js'></script>
+        <script src='js/bootstrap.min.js'></script>
+
+
+    </head>
+    <body>
+      <div class ="container">
+        <div class="getStarted">
+          Główcześ & Fadro Inc.
+        </div>
+        <div class="turtle">
+          <img src="img/elo.png">
+        </div>
+      </div>
+    </body>
+</html>
