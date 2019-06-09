4cc28f522cffdc3ebea720f0cd19c65ab7bff256
F4dr0 <41900341+F4dr0@users.noreply.github.com>
2019-06-09 17:47:18 +0200

    elo

--- routes/web.php
+++ routes/web.php
@@ -12,24 +12,15 @@
 */
 
 Route::get('/', 'PagesControler@index');
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> e295011f011e4899c3aec71cfe44bb62cb27abab
+
 Route::get('/admin', 'PagesControler@panel');
 Route::get('/admin/newCourse', 'PagesControler@newCourse');
 Route::get('/admin/{id}/edit', 'CourseController@edit');
 Route::get('/courses', 'PagesControler@courses');
-<<<<<<< HEAD
-Route::get('/faq', 'PagesControler@faq');
 
-=======
 
-=======
 Route::get('/noElo', 'PagesControler@addCourse');
 Route::get('/faq', 'PagesControler@faq');
->>>>>>> e1fb838db16e9ee08d04638540c723ba66f870c6
->>>>>>> e295011f011e4899c3aec71cfe44bb62cb27abab
 Route::post('form', 'CourseController@create')->name('form.create');
 Route::post('update', 'CourseController@update')->name('form.update');
 Route::get('/admin/delete'  , 'CourseController@delete')->name('form.delete');
